e848395f55d81e64aaa27854b71a79304580cd90
unknown <MohammadRasel>
2024-10-29 20:51:17 +0600

    Completed the final task of Front End

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,29 @@
-
-import { useLoaderData } from 'react-router-dom';
-import './App.css';
-import CoffeeCard from './components/CoffeeCard';
+import { useLoaderData } from "react-router-dom";
+import "./App.css";
+import CoffeeCard from "./components/CoffeeCard";
+import { useState } from "react";
 
 function App() {
-
-  const coffees = useLoaderData();
+  const loadedCoffees = useLoaderData();
+  const [coffees, setCoffees] = useState(loadedCoffees);
 
   return (
-    <>      
-      <h1 className='text-6xl  '>Hot And Cold Coffee: {coffees.length}</h1>
-      {
-        coffees.map(coffee => <CoffeeCard
+    <div className=" m-20">
+      <h1 className="text-6xl text-center text-purple-600 my-20 ">
+        Hot And Cold Coffee: {coffees.length}
+      </h1>
+      <div className=" grid md:grid-cols-2 gap-4">
+        {coffees.map((coffee) => (
+          <CoffeeCard 
           key={coffee._id}
           coffee={coffee}
-        ></CoffeeCard>)
-      }
-
-    </>
-  )
+          coffees={coffees}
+          setCoffees={setCoffees}
+          ></CoffeeCard>
+        ))}
+      </div>
+    </div>
+  );
 }
 
-export default App
+export default App;
--- src/components/CoffeeCard.jsx
+++ src/components/CoffeeCard.jsx
@@ -1,22 +1,72 @@
+import { Link } from "react-router-dom";
+import Swal from "sweetalert2";
 
+const CoffeeCard = ({ coffee, coffees, setCoffees }) => {
+  const {_id, name, quantity, supplier, taste, category, details, photo } = coffee;
 
-const CoffeeCard = ({coffee}) => {
-
-  const {name, quantity, supplier, taste, category, details, photo} = coffee;
+  const handleDelete = _id => {
+    console.log(_id);
+    Swal.fire({
+      title: "Are you sure?",
+      text: "You won't be able to revert this!",
+      icon: "warning",
+      showCancelButton: true,
+      confirmButtonColor: "#3085d6",
+      cancelButtonColor: "#d33",
+      confirmButtonText: "Yes, delete it!"
+    }).then((result) => {
+      if (result.isConfirmed) {
       
+
+
+
+        fetch(`http://localhost:5000/coffee/${_id}`, {
+          method:'DELETE'
+        })
+
+            .then(res => res.json())
+            .then(data => {
+              console.log(data);
+              if(data.deletedCount > 0){
+                Swal.fire({
+                        title: "Deleted!",
+                        text: "Your Coffee has been deleted.",
+                        icon: "success"
+                      });
+                      const remaining = coffees.filter(coffee => coffees._id !==_id);
+                      setCoffees(remaining);
+              }
+            })
+        
+          }
+         })
+         
+      
+  }
+
+
   return (
-    <div className="card card-side bg-base-100 shadow-xl">
+    <div className="card card-side bg-base-100 shadow-xl ">
       <figure>
-        <img
-          src={photo}
-          alt="Movie"
-        />
+        <img src={photo} alt="Movie" />
       </figure>
-      <div className="card-body">
-        <h2 className="card-title">New movie is released!</h2>
-        <p>Click the button to watch on Jetflix app.</p>
+      <div className=" flex justify-between w-full p-4">
+        <div> 
+          <h2 className="card-title">Name : {name}</h2>
+          <p>{quantity}</p>
+          <p>{supplier}</p>
+          <p>{taste}</p>
+        </div>
         <div className="card-actions justify-end">
-          <button className="btn btn-primary">Watch</button>
+          <div className="join join-vertical space-y-4">
+            <button className="btn">View</button>
+            <Link to={`updateCoffee/${_id}`}>
+            <button className="btn">Edit</button>
+            </Link>
+            <button 
+              onClick={() => handleDelete(_id)}
+            className="btn bg-red-500">X</button>
+          </div>
         </div>
       </div>
     </div>
